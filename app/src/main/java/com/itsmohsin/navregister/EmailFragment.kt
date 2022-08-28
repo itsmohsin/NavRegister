a0166d23533b850fac210cfe4d5205579433675f
Mohsin Khan <mohsinsmannat@gmail.com>
2022-08-28 16:42:27 +0530

    Added fragments, animations and logic to App

--- app/src/main/java/com/itsmohsin/navregister/EmailFragment.kt
+++ app/src/main/java/com/itsmohsin/navregister/EmailFragment.kt
@@ -1,59 +1,42 @@
 package com.itsmohsin.navregister
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import com.itsmohsin.navregister.databinding.FragmentEmailBinding
+import java.util.regex.Pattern
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EmailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EmailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentEmailBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_email, container, false)
+        binding = FragmentEmailBinding.inflate(inflater, container, false)
+        val inputName = requireArguments().getString("user_name")
+        binding.btnSubmit.setOnClickListener {
+            if(!TextUtils.isEmpty(binding.etEmail.text)){
+                if(Patterns.EMAIL_ADDRESS.matcher(binding.etEmail.text).matches()){
+                    val bundle = bundleOf(
+                        "user_name" to inputName,
+                        "user_email" to binding.etEmail.text.toString()
+                    )
+                    it.findNavController().navigate(R.id.action_emailFragment_to_welcomeFragment,bundle)
+                }else
+                    Toast.makeText(activity,"Please enter a correct email address", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(activity,"Email field is empty", Toast.LENGTH_LONG).show()
+            }
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EmailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EmailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
